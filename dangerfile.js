31de934c4970ec4211ba5012285f5dc8ecb7df8e
Ladislav Prskavec <ladislav@prskavec.net>
2018-04-16 14:10:55 +0200

    fix: update dangerfile to check commit message format

--- dangerfile.js
+++ dangerfile.js
@@ -1,18 +1,29 @@
 const {danger, warn} = require('danger')
-const lint = require('@commitlint/lint')
+const load = require('@commitlint/load');
+const lint = require('@commitlint/lint');
+
+const CONFIG = {
+  extends: ['@commitlint/config-conventional']
+};
 
 // No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
 
+var errorCount = 0;
 // validate commit message in PR if it conforms conventional change log, notify if it doesn't.
 var messageConventionValid = danger.git.commits.reduce(function (acc, value) {
-  var valid = lint(value.message);
-  return valid && acc;
+  load(CONFIG)
+    .then(opts => lint(value.message, opts.rules, opts.parserPreset ? { parserOpts: opts.parserPreset.parserOpts } : {}))
+    .then(report => {
+      console.log(report);
+      return report.valid && acc;
+    });
+  ;
 }, true);
 
 if (!messageConventionValid) {
   warn('commit message does not follows conventional change log (' + ++errorCount + ')');
-  markdown('> (' + errorCount + ') : RxJS uses conventional change log to generate changelog automatically. It seems some of commit messages are not following those, please check [contributing guideline](https://github.com/ReactiveX/rxjs/blob/master/CONTRIBUTING.md#commit-message-format) and update commit messages.');
+  markdown('> (' + errorCount + ') : Project uses conventional change log to generate changelog automatically. It seems some of commit messages are not following those, please update commit messages.');
 }
